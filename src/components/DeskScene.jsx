8b2da305deaa3b99174f323c600be3fe0051254a
vvanessaww <vvanessaww@users.noreply.github.com>
2026-02-12 03:38:34 +0000

    Remove glow outline from objects and increase subtitle opacity

--- src/components/DeskScene.jsx
+++ src/components/DeskScene.jsx
@@ -8,22 +8,6 @@ function InteractiveObject({ children, name, position, rotation, onClick }) {
   const [hovered, setHovered] = useState(false)
   const meshRef = useRef()
 
-  // Define bounding box dimensions for each object type
-  const getBoundingBox = (name) => {
-    switch(name) {
-      case 'laptop':
-        return { size: [0.85, 0.6, 0.5], offset: [0, 0.3, 0] }
-      case 'notebook':
-        return { size: [0.35, 0.05, 0.45], offset: [0, 0.02, 0] }
-      case 'postcard':
-        return { size: [0.38, 0.02, 0.25], offset: [0, 0.01, 0] }
-      default:
-        return { size: [0.5, 0.5, 0.5], offset: [0, 0, 0] }
-    }
-  }
-
-  const boundingBox = getBoundingBox(name)
-
   return (
     <group
       ref={meshRef}
@@ -45,21 +29,6 @@ function InteractiveObject({ children, name, position, rotation, onClick }) {
       }}
       scale={hovered ? 1.05 : 1}
     >
-      {/* Subtle white outline glow matching object shape */}
-      <mesh position={boundingBox.offset}>
-        <boxGeometry args={[
-          boundingBox.size[0] * 1.15,
-          boundingBox.size[1] * 1.15,
-          boundingBox.size[2] * 1.15
-        ]} />
-        <meshBasicMaterial
-          color="#ffffff"
-          transparent
-          opacity={hovered ? 0.25 : 0.12}
-          depthWrite={false}
-          side={THREE.BackSide}
-        />
-      </mesh>
       {children}
     </group>
   )
